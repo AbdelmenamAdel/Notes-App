42c6bd0620d1fdfe90006f0802455188ee721f0b
AbdelmenamAdel <127325365+AbdelmenamAdel@users.noreply.github.com>
2024-02-16 01:04:50 +0200

    delete modal progress hud temporary

--- lib/Views/widget/add_note_buttom_sheet.dart
+++ lib/Views/widget/add_note_buttom_sheet.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/Views/widget/add_note_form.dart';
 import 'package:notes_app/cubits/add_note_cubit.dart/add_note_cubit.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
   const AddNoteButtomSheet({super.key});
@@ -10,26 +9,23 @@ class AddNoteButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider<AddNoteCubit>(
       create: (context) => AddNoteCubit(),
-      child: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: BlocConsumer<AddNoteCubit, AddNoteState>(
-          listener: (context, state) {
-            if (state is AddNoteFailureState) {
-              print('Failed ${state.errMessage}');
-            }
-            if (state is AddNoteSuccessState) {
-              Navigator.pop(context);
-            }
-          },
-          builder: (context, state) {
-            return ModalProgressHUD(
-              inAsyncCall: state is AddNoteLoadingState ? true : false,
-              child: const SingleChildScrollView(
-                child: AddNoteForm(),
-              ),
-            );
-          },
-        ),
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state is AddNoteFailureState) {
+            print('Failed ${state.errMessage}');
+          }
+          if (state is AddNoteSuccessState) {
+            Navigator.pop(context);
+          }
+        },
+        builder: (context, state) {
+          return const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: SingleChildScrollView(
+              child: AddNoteForm(),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/Views/notes_view.dart';
-import 'package:notes_app/cubits/add_note_cubit.dart/add_note_cubit.dart';
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
