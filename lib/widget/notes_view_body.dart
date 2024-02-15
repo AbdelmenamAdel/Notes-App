b7fe9d80fdc7ae2649c1ba6fd807fef4c6287874
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-02-15 14:31:00 +0200

    init hive and open notes box

---
+++ lib/widget/notes_view_body.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/Views/widget/custom_app_bar.dart';
+import 'package:notes_app/Views/widget/custom_note_item.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 24,
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(
+            title: 'Notes',
+            icon: Icons.search,
+          ),
+          SizedBox(height: 10),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
